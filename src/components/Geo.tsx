d4fcdf396dc19e86d599b8737897056f30935c0e
Alclides <alclidessantos010@gmail.com>
2025-04-10 13:31:50 -0300

    Primeiro Commit

---
+++ src/components/Geo.tsx
@@ -0,0 +1,17 @@
+export const Square = () => {
+    return (
+        <div className="w-52 h-52 bg-orange-700 text-white text-center">
+            texto qualquer
+        </div>
+    )
+}
+
+export const Circle = () => {
+    return (
+        <div className="w-52 h-52 bg-orange-700 text-white rounded-full text-center" >
+            outro texto
+        </div>
+
+    )
+    
+}
